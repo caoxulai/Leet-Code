86d1d81ed4386ca962fea5e6b633527dba379046
Bryce <caoxulaiproject@gmail.com>
2018-01-25 09:08:30 -0800

    102. Binary Tree Level Order Traversal

---
+++ src/leetcode/levelOrder_102.java
@@ -0,0 +1,39 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *
+ * @author Bryce
+ */
+public class levelOrder_102 {
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> res = new ArrayList<>();
+            if(root == null)    return res;
+            
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while(!queue.isEmpty()){
+                List<Integer> list = new ArrayList<>();
+                int size = queue.size();
+                for(int i=0; i<size; i++){
+                    TreeNode node = queue.poll();
+                    list.add(node.val);
+                    if(node.left != null)   queue.offer(node.left);
+                    if(node.right != null)   queue.offer(node.right);
+                }
+                res.add(list);
+            }
+            return res;
+        }
+    }
+}
