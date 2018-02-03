ebed4ad679d9e7ce3e78b63af4d0eebb61adeb45
Bryce <caoxulaiproject@gmail.com>
2018-02-02 23:54:51 -0800

    20180202 Submissions

---
+++ src/leetcode/generateTrees_095.java
@@ -0,0 +1,45 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Bryce
+ */
+public class generateTrees_095 {
+    class Solution {
+        public List<TreeNode> generateTrees(int n) {
+            if(n==0)    return new ArrayList<>();
+            return generateTrees(1, n);
+        }
+        
+        public List<TreeNode> generateTrees(int i, int j) {
+            List<TreeNode> list = new ArrayList<>();
+            if(i > j){
+                list.add(null);
+            }else if(i == j){
+                list.add(new TreeNode(i));
+            }else{
+                for(int root=i; root<=j; root++){
+                    List<TreeNode> left = generateTrees(i, root-1);
+                    List<TreeNode> right = generateTrees(root+1, j);
+                    for(TreeNode l: left){
+                        for(TreeNode r: right){
+                            TreeNode node = new TreeNode(root);
+                            node.left = l;
+                            node.right = r;
+                            list.add(node);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
