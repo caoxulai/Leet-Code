35d8a2f794a71cd84a540732e11aa6b85b9bf47b
Xulai Cao <xulai.cao@sap.com>
2018-02-04 14:17:08 -0800

    20180204 Company change

---
+++ src/leetcode/inorderTraversal_094.java
@@ -0,0 +1,30 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author I852047
+ */
+public class inorderTraversal_094 {
+    class Solution {
+        List<Integer> res = new ArrayList<>();
+        public List<Integer> inorderTraversal(TreeNode root) {
+            inorder(root);
+            return res;
+        }
+        
+        private void inorder(TreeNode node){
+            if(node == null)    return;
+            inorder(node.left);
+            res.add(node.val);
+            inorder(node.right);
+        }
+    }
+}
