090f4c221c81e65365ecd8bd9fc7e4f1825108d5
Xulai <xulaicao@amazon.com>
2020-10-22 22:34:31 -0700

    20201022 Submissions

---
+++ src/leetcode/trimBST_669.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class trimBST_669 {
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root == null)    return null;
+
+        if(root.val < low)
+            return trimBST(root.right, low, high);
+        else if(root.val > high)
+            return trimBST(root.left, low, high);
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
