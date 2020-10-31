21224e46529c3c37bc233b706c31b5a8dfaceb61
Xulai <xulaicao@amazon.com>
2020-10-31 01:04:14 -0700

    20201030 Submissions

---
+++ src/leetcode/deleteNode_450.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class deleteNode_450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null)    return null;
+        else if(root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+        else if(root.val > key) {
+            root.left = deleteNode(root.left, key);
+        }
+        else {
+            if(root.left == null)   return root.right;
+            else if(root.right == null) return root.left;
+
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node){
+        while(node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+}
