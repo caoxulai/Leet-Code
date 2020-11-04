5fec3cf14b0cc6406b3e2c69505ce1eec9c240a8
Xulai <xulaicao@amazon.com>
2020-11-03 21:46:34 -0800

    20201104 Submissions

---
+++ src/leetcode/allPossibleFBT_894.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class allPossibleFBT_894 {
+    // Use memo to store calculated results.
+    private Map<Integer, List<TreeNode>> memo = new HashMap();
+
+    public List<TreeNode> allPossibleFBT(int N) {
+        if(memo.get(N) != null)
+            return memo.get(N);
+
+        List<TreeNode> ans = new ArrayList<>();
+        if(N == 1)
+            ans.add(new TreeNode(0));
+        else if(N % 2 == 0)
+            return ans;
+
+        for(int i=1; i<= N-2; i += 2) {
+            for(TreeNode left : allPossibleFBT(i)) {
+                for(TreeNode right : allPossibleFBT(N-1-i)) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = left;
+                    root.right = right;
+                    ans.add(root);
+                }
+            }
+        }
+        memo.put(N, ans);
+        return ans;
+    }
+}
