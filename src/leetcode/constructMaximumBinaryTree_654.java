4a6db2247afa596deba0c18637d8817a175ecf07
Xulai <xulaicao@amazon.com>
2020-11-12 22:09:24 -0800

    20201114 Submissions

---
+++ src/leetcode/constructMaximumBinaryTree_654.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class constructMaximumBinaryTree_654 {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return constructMaximumBinaryTree(nums, 0, nums.length);
+    }
+
+    private TreeNode constructMaximumBinaryTree(int[] nums, int s, int e) {
+        if(s >= e)  return null;
+        int maxIndex = findMax(nums, s, e);
+        TreeNode node = new TreeNode(nums[maxIndex]);
+        node.left = constructMaximumBinaryTree(nums, s, maxIndex);
+        node.right = constructMaximumBinaryTree(nums, maxIndex + 1, e);
+        return node;
+    }
+
+    private int findMax(int[] nums, int s, int e) {
+        int max = Integer.MIN_VALUE, index = -1;
+        for(int i=s; i<e; i++) {
+            if(max < nums[i]) {
+                max = nums[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
---
+++ src/leetcode/sumRootToLeaf_1022.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class sumRootToLeaf_1022 {
+    private int ans = 0;
+
+    public int sumRootToLeaf(TreeNode root) {
+        if(root == null)    return 0;
+        dfs(root, 0);
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int val){
+        val = (val << 1) + node.val;
+        if(node.left == null && node.right == null)
+            ans += val;
+        else{
+            if(node.left != null)   dfs(node.left, val);
+            if(node.right != null)   dfs(node.right, val);
+        }
+    }
+}
