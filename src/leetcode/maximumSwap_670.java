20fc58a6eb2860f28b5d450e8035a8a17dd31ead
Xulai <xulaicao@amazon.com>
2020-12-02 01:25:04 -0800

    20201201 Submissions

---
+++ src/leetcode/maximumSwap_670.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class maximumSwap_670 {
+    public int maximumSwap(int num) {
+        char[] A = Integer.toString(num).toCharArray();
+
+        int[] last = new int[10];
+        for (int i = 0; i < A.length; i++) {
+            last[A[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            for (int d = 9; d > A[i] - '0'; d--) {
+                if (last[d] > i) {
+                    // there is bigger number on smaller digit
+                    char tmp = A[i];
+                    A[i] = A[last[d]];
+                    A[last[d]] = tmp;
+                    return Integer.valueOf(new String(A));
+                }
+            }
+        }
+        return num;
+    }
+}
