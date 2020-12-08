6c2f659d5b66d0446f7d3d509fad649e076fb42d
Xulai <xulaicao@amazon.com>
2020-12-07 21:31:07 -0800

    20201207 Submissions

---
+++ src/leetcode/isIdealPermutation_775.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class isIdealPermutation_775 {
+    public boolean isIdealPermutation(int[] A) {
+        for(int i=0; i<A.length; i++) {
+            if(Math.abs(A[i] - i) > 1)
+                return false;
+        }
+        return true;
+    }
+}
