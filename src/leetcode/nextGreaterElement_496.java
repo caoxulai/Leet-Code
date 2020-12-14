30c037bf59a5de8b0a261723e91df3f72dc7e39b
Xulai <xulaicao@amazon.com>
2020-12-13 21:25:54 -0800

    20201214 Submissions

---
+++ src/leetcode/nextGreaterElement_496.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class nextGreaterElement_496 {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
+    }
+}
