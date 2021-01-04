b4da0e8258ce9b0bbf640d2e8fdbe5030d4a602f
Xulai <xulaicao@amazon.com>
2021-01-03 23:46:29 -0800

    20210103

---
+++ src/leetcode/find132pattern_456.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class find132pattern_456 {
+    public boolean find132pattern(int[] nums) {
+        if (nums.length < 3)
+            return false;
+        Stack<Integer> stack = new Stack<>();
+
+        int[] min = new int[nums.length];
+        min[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            min[i] = Math.min(min[i - 1], nums[i]);
+
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] > min[j]) {
+                while (!stack.isEmpty() && stack.peek() <= min[j])
+                    stack.pop();
+                if (!stack.isEmpty() && stack.peek() < nums[j])
+                    return true;
+                stack.push(nums[j]);
+            }
+        }
+        return false;
+    }
+}
