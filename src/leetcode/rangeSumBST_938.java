edb2e5a60494ee835e0ee77ad4fb57d834f948d7
Xulai <xulaicao@amazon.com>
2021-01-06 01:31:31 -0800

    20210105

---
+++ src/leetcode/rangeSumBST_938.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class rangeSumBST_938 {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null)    return 0;
+        else if (root.val < low)
+            return rangeSumBST(root.right, low, high);
+        else if (root.val > high)
+            return rangeSumBST(root.left, low, high);
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+}
