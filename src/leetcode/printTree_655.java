ba422a02f89d0538c74fc3522847e4179cf59638
Xulai <xulaicao@amazon.com>
2021-01-09 03:57:37 -0800

    20210108

---
+++ src/leetcode/printTree_655.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class printTree_655 {
+    public List<List<String>> printTree(TreeNode root) {
+        List<List<String>> ans = new ArrayList<>();
+        int height = height(root);
+        for(int i=0; i<height; i++) {
+            List<String> currentLevel = new ArrayList<>();
+            for(int j=0; j < Math.pow(2, height) - 1; j++) {
+                currentLevel.add("");
+            }
+            ans.add(currentLevel);
+        }
+
+        updateMatrix(ans, root, 0, (int)Math.pow(2, height-1) - 1, (int) Math.pow(2, height-1));
+        return ans;
+    }
+
+    private void updateMatrix(List<List<String>> ans, TreeNode node, int row, int col, int width){
+        if(node == null || width == 0)
+            return;
+        ans.get(row).set(col, String.valueOf(node.val));
+        updateMatrix(ans, node.left, row+1, col-width/2, width/2);
+        updateMatrix(ans, node.right, row+1, col+width/2, width/2);
+    }
+
+
+    private int height(TreeNode node){
+        if(node == null)    return 0;
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+        return 1 + Math.max(left, right);
+    }
+}
---
+++ src/leetcode/tree2str_606.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class tree2str_606 {
+    public String tree2str(TreeNode t) {
+        if(t == null)
+            return "";
+        if(t.left == null && t.right == null)
+            return String.valueOf(t.val);
+        else if(t.right == null) {
+            return t.val + "(" + tree2str(t.left) + ")";
+        }
+        return t.val + "(" + tree2str(t.left) + ")" + "(" + tree2str(t.right) + ")";
+    }
+}
