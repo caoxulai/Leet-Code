aa82d784a2de242449e8e95b3c45535ea096fb45
Xulai <xulaicao@amazon.com>
2021-01-20 00:45:40 -0800

    20210119

---
+++ src/leetcode/largestValues_515.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import leetcode.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class largestValues_515 {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null)
+            return ans;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for(int i=0; i<size; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+                if(node.left != null)
+                    queue.offer(node.left);
+                if(node.right != null)
+                    queue.offer(node.right);
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
